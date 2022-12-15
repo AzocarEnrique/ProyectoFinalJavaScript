fa50ab4077936f4beaca8324c7a9b989d03a871a
AzocarEnrique <azocarinost@gmail.com>
2022-12-15 18:03:36 -0300

    HOLA

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState, useEffect} from "react";
 
 function App() {
+    const [data, setData] = useState(null)
+
+    useEffect(() => {
+        getData();
+    }, [])
+
+  const getData = async () => {
+      try {
+        const request = await fetch('https://dummyjson.com/products');
+        const response = await request.json();
+        console.log(response.products)
+        setData(response.products);
+      } 
+      catch (error) {
+        console.error(error)
+      }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Pokemon List</h1>
+      <div >
+        {data && data.map((item) => (
+          <div className="algo">
+            <li key={item.title} className="items">
+              <p key={item.title+"title"}>{item.title}</p>
+            </li> 
+            <li key={item.title} className="items">
+              <p key={item.title+"description"}>{item.description}</p>
+            </li> 
+            <li key={item.title} className="items">
+              <p key={item.title+"price"}>{item.price}</p>
+            </li> 
+            <img src={item.thumbnail}></img>
+          </div>
+        ))}
+      </div>
     </div>
+      
   );
 }
-
 export default App;
